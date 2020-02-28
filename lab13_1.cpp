5c6aea1012a6dbc02dd80102d7155c8b0343ea1f
PackYokohama <59277976+PackYokohama@users.noreply.github.com>
2020-02-28 12:37:26 +0700

    Update lab13_1.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -50,3 +50,29 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
+				
+			}
+		}
+			for (int j = 0; j < N; j++){
+			if ( d[j] == s && j<i+1 && d[j+1] != s){
+				cout << "\b[" << d[j] << "]" << " ";
+			}else{
+				cout << d[j] << " " ;
+			}
+		}
+		cout << "\n";
+	}
+}
+
+int main(){
+	int a[10] = {12,25,30,44,2,0,4,7,55,25};
+	cout << "Input Array:";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";
+	cout << "\n\n";
+	
+	insertionSort(a,10);
+	
+	cout << "\nSorted Array:";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+}
